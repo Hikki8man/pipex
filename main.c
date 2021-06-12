8c9baeb27c3abc078756cbc9f44152a886600386
Johan Chevet <jchevet@z1r7p1.42lyon.fr>
2021-06-12 15:34:47 +0200

    bien avancé

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-	printf("Hello, World!\n");
+int main(int ac, char **av)
+ {
+	int id;
+	id = fork();
+
+	printf("Hello, World! from id : %d\n", id);
 	return 0;
 }
