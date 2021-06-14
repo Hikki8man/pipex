63cf3ad4bc9f24b302dd07ad0d05527257bf3451
Johan Chevet <jchevet@z1r7p1.42lyon.fr>
2021-06-14 15:49:54 +0200

    as

--- main.c
+++ main.c
@@ -1,12 +1,184 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include "libft/include/libft.h"
+#include <fcntl.h>
+#include <errno.h>
 
-int main(int ac, char **av)
+//fd[0] = READ
+//fd[1] = WRITE
+
+typedef struct s_cmd
+{
+	char	*name;
+	char	*path;
+	char	**param;
+	int 	nb_param;
+	struct s_cmd *next;
+}t_cmd;
+
+t_cmd	*cmd_lst_last(t_cmd *cmd_l)
+{
+	while (cmd_l && cmd_l->next)
+		cmd_l = cmd_l->next;
+	return (cmd_l);
+}
+
+
+t_cmd *new_cmd(void)
+{
+	t_cmd *cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (cmd == NULL)
+		return (NULL);
+	cmd->param = NULL;
+	cmd->name = 0;
+	cmd->path = NULL;
+	cmd->nb_param = 0;
+	cmd->next = NULL;
+	return (cmd);
+}
+void	add_back(t_cmd **cmd_list, t_cmd *new_cmd)
+{
+	t_cmd	*last;
+
+	if (*cmd_list == NULL)
+		*cmd_list = new_cmd;
+	else
+	{
+		last = cmd_lst_last(*cmd_list);
+		last->next = new_cmd;
+	}
+}
+
+void exit_perror(char *name)
+{
+	perror(name);
+	exit(EXIT_FAILURE);
+}
+
+char	**split_env_path(char **envp)
+{
+	int	i;
+	char **paths_tab;
+
+	i = 0;
+	while (ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	envp[i] += 5;
+	paths_tab = ft_split(envp[i], ':');
+	return (paths_tab);
+}
+
+void find_cmd_path(char **paths_tab, t_cmd *cmd_list, char **argv)
+{
+
+}
+
+int 	nb_of_param(char **param)
+{
+	int	i;
+
+
+	i = 0;
+	while (param[i])
+		i++;
+	return (i);
+}
+
+void	get_cmd(int ac, char **av, t_cmd **cmd_l)
+{
+	t_cmd *cmd;
+	int i;
+
+	i = 2;
+	while (i < ac - 1)
+	{
+//		printf("%s\n", av[i]);
+		cmd = new_cmd();
+		if (!cmd)
+			exit_perror("new_cmd");
+		cmd->param = ft_split(av[i], ' ');
+		cmd->name = *cmd->param;
+		cmd->nb_param = nb_of_param(cmd->param);
+		add_back(cmd_l, cmd);
+		i++;
+	}
+}
+
+int main(int argc, char **argv, char **envp)
  {
-	int id;
-	id = fork();
+	int pid1;
+	int pid2;
+	int fd[2];
+	char *line;
+	char **paths_tab;
+	t_cmd *cmd_list;
 
-	printf("Hello, World! from id : %d\n", id);
+	cmd_list = NULL;
+	char *arr[] = {
+			 "ls",
+			 "-la",
+			 NULL
+	};
+	paths_tab = split_env_path(envp);
+	get_cmd(argc, argv, &cmd_list);
+	int i;
+
+	while (cmd_list)
+	{
+		i = 0;
+		printf("Name: %s\nNB OF PARAM: %d\n", cmd_list->name,
+		 cmd_list->nb_param);
+		while (cmd_list->param[i])
+		{
+			printf("param %d: %s\n", i + 1, cmd_list->param[i]);
+			i++;
+		}
+		cmd_list = cmd_list->next;
+	}
+//	while (paths_tab[i])
+//	{
+//		printf("%s\n", paths_tab[i]);
+//		i++;
+//	}
+	 int std_out = dup(STDIN_FILENO);
+//	if (pipe(fd) == -1)
+//		exit_perror("pipe");
+//	pid1 = fork();
+//	if (pid1 == -1)
+//		exit_perror("fork");
+//	if (pid1 == 0) {
+//
+//		dup2(fd[1], STDOUT_FILENO);
+//		close(fd[0]);
+//		close(fd[1]);
+//		execve("ls", arr, envp);
+//	}
+//	 pid2 = fork();
+//	 if (pid2 == -1)
+//		 exit_perror("fork");
+//	 if (pid2 == 0)
+//	 {
+//	 	waitpid(pid1, NULL, 0);
+//	 	dup2(fd[0], STDIN_FILENO);
+//	 	close(fd[0]);
+//	 	close(fd[1]);
+//		 int ret = execlp(
+//				 "/usr/bin/grep",
+//				 "grep",
+//				 "libft",
+//				 NULL
+//		 );
+//		 if (ret == -1) {
+//			 perror("execlp");
+//			 exit(EXIT_FAILURE);
+//		 }
+//
+//	 }
+//	close(fd[1]);
+//	close(fd[0]);
+//	waitpid(pid1, NULL, 0);
+//	waitpid(pid2, NULL, 0);
 	return 0;
 }
